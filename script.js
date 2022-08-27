d5f3f4436600842cc75e0c8e7b1ca9b8a8b3b28f
Esdras Antero <esdrasanterofontes@gmail.com>
2022-08-27 11:19:53 -0300

    Projeto Finalizado + Bônus

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-//Nome do usuário (falta deixar bonito como no bonus)
- let nomeDoUsuario = {
-     name: prompt("Digite seu nome!")
- }
+//Nome do usuário
+function sumirTela (inputEntrar) {
+    const gif = document.querySelector(".gif")
+    const h1Carregamento = document.querySelector(".tela-entrada h1")
+    const inputNome = document.querySelector(".nome")
+
+    if (inputNome.value !== '') {
+        inputNome.classList.add("esconder")
+        inputEntrar.classList.add("esconder")
+        gif.classList.remove("esconder")
+        h1Carregamento.classList.remove("esconder")
+    }
+
+    let nomeDoUsuario = {
+        name: inputNome.value
+        }
+
+    enviarNome(nomeDoUsuario)
+
+    setTimeout (() => {
+    const telaEntrada = document.querySelector(".back-tela-entrar")
+    telaEntrada.classList.add("esconder")
+}, 4000)
+
+    setTimeout(() => {
+        alert("Para melhor experiência, primeiro escreva a mensagem no campo de texto, para  depois selecionar o Destinatário e a Visibilidade")
+    }, 5000);
+}
 
 //Entrar na sala
-function enviarNome () {
+function enviarNome (nomeDoUsuario) {
     const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeDoUsuario)
     promise.then(manterConexao)
     promise.catch(deuErro)
+
+    document.addEventListener("keypress", function(e){
+        if (e.key === "Enter") {
+            adicionarMensagem()
+        }
+    }) 
 }
 
 //Manter a conexão com a sala
 function manterConexao () {
+    const inputNome = document.querySelector(".nome")
+    let nomeDoUsuario = {
+        name: inputNome.value
+        }
+
     setInterval (() => {
         const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeDoUsuario)
+        promise.catch(deuErroConexao)
     }, 5000)
     
     pegarMensagem()
@@ -37,24 +73,25 @@ function buscarParticipantes () {
 function criarListaDeUsuarios (listaUsuarios){
     const ulUsuarios = document.querySelector(".participantes")
     let nomeDosUsuarios = listaUsuarios.data
- 
-     if ((ulUsuarios.childElementCount + nomeDosUsuarios.length) > nomeDosUsuarios.length) {
-         ulUsuarios.innerHTML = ` <div class="usuarios" data-identifier="participant">
-         <ion-icon name="people-sharp"></ion-icon>
-         <span class="usuario" data-identifier="participant" >Todos</span>
+    
+    if ((ulUsuarios.childElementCount + nomeDosUsuarios.length) > nomeDosUsuarios.length) {
+        ulUsuarios.innerHTML = ` <div class="usuarios selecionado" onclick="escolherUsuariosTodos(this)" data-identifier="participant">
+        <ion-icon name="people-sharp"></ion-icon>
+        <span class="usuario" data-identifier="participant" >Todos</span>
 
-         <div class="check">
-             <ion-icon name="checkmark-sharp"></ion-icon>
-         </div>
-     </div>`
-     }
- 
+        <div class="check">
+            <ion-icon name="checkmark-sharp"></ion-icon>
+        </div>
+    </div>`
+    }
+
      for (let i = 0; i < nomeDosUsuarios.length; i++) {
          const usuario = nomeDosUsuarios[i]
          const nomeDoUsuario = usuario.name
  
          const divUsuario = document.createElement ('div');
          divUsuario.classList.add("usuarios")
+         divUsuario.addEventListener("click", escolherUsuario);
  
          divUsuario.innerHTML = `
          <ion-icon name="people-sharp"></ion-icon>
@@ -91,43 +128,67 @@ function renderizarMensagem (response){
         batepapo.appendChild(divMensagem);
     }
 
-    let ultimaMensagem = batepapo.lastChild.scrollIntoView();
+    let ultimaMensagem = batepapo.lastChild.scrollIntoView({behavior:"smooth"});
 }
 
 //Adicionar o que eu escrevo nas mensagens que estão no servidor
-function adicionarMensagem (inputChat){
+function adicionarMensagem () {
     let data = new Date();
     let horas = data.getHours();
     let minutos = data.getMinutes();
     let segundos = data.getSeconds();
 
+    const opcaoEscolhidaTexto = document.querySelector('.visibilidade .selecionado span').innerHTML
+    const opcaoEscolhidaUsuario = document.querySelector('.participantes .selecionado span').innerHTML
+    const inputNome = document.querySelector(".nome")
+
+    let nomeDoUsuario = {
+        name: inputNome.value
+        }
     let horario = horas + ":" + minutos + ":" + segundos
     const conteudo = document.querySelector('.chat').value
     const remetente = nomeDoUsuario.name
-    const destinatario = 'Todos'  // pra colocar diferente é bonus
+    const destinatario = opcaoEscolhidaUsuario
+    let tipo = ''
+
+    if (opcaoEscolhidaTexto === "Público") {
+        tipo = "message"
+    } else {
+        tipo = "private_message"
+    }
 
     let mensagem = {
         from: remetente,
-        to: destinatario, //pra colocar diferente é bonus
+        to: destinatario,
         text: conteudo,
-        type: "message", // pra colocar diferente é bonus
+        type: tipo,
         time: horario
     }
 
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem)
     promise.catch(deuErroMensagem)
+
+    document.querySelector('.chat').value = ''
 }   
 
 //Respostas para possíveis erros
-function deuErro(erro){
+function deuErro(erro) {
     console.log(erro)
 
     if (erro.response.status === 400){
-        alert(`Nome de usuário já está em uso! Escolha outro nickname ou certifique-se de que você não está logado.
-(Lembre-se, você só pode usar o chat se tiver um nickname!)`)
+        alert(`Nome de usuário inválido!
+Por favor, tente novamente \uD83D\uDE0A
+
+Essa mensagem pode ter aparecido por 2 motivos:
+· Você não digitou o seu nome
+· Alguém já está registrado com esse nome`)
 
         location.reload()
     }
+}
+
+function deuErroConexao(erro) {
+        location.reload()
 }
 
 function deuErroMensagem(erro) {
@@ -142,25 +203,70 @@ function deuErroMensagem(erro) {
 
 //<============================================>
 
-enviarNome()
 setInterval(pegarMensagem, 3000)
 setInterval(buscarParticipantes, 10000)
 
 //<============================================>
 
 //Funções do Bônus
-function sumir(sidebar) {
-    sidebar.classList.add("esconder")
+function sumir() {
+    const backgroundsidebar = document.querySelector('.background-sidebar')
+    backgroundsidebar.classList.add("esconder")
 
+    const partepreta = document.querySelector('.parte-preta')
+    partepreta.classList.add("esconder")
 }
 
 function aparecer() {
-    const sidebar = document.querySelector('.background-sidebar')
-    sidebar.classList.remove("esconder")
+    const backgroundsidebar = document.querySelector('.background-sidebar')
+    backgroundsidebar.classList.remove("esconder")
+
+    const partepreta = document.querySelector('.parte-preta')
+    partepreta.classList.remove("esconder")
 }
 
-function escolher() {
-    const divCheck = document.querySelector('.check')
-    console.log (divCheck)
-    divCheck.classList.add("aparecer")
+document.querySelector('.parte-preta').addEventListener("click", sumir);
+
+function escolherUsuario ({target}) {
+    const opcaoEscolhida = document.querySelector('.participantes .selecionado')
+
+    if (opcaoEscolhida !== null){
+        opcaoEscolhida.classList.remove('selecionado')
+    }
+
+    target.classList.add("selecionado")
+
+    textoInput()
+}
+
+function escolherUsuarioTodos (todos) {
+    const opcaoEscolhida = document.querySelector('.participantes .selecionado')
+
+    if (opcaoEscolhida !== null){
+        opcaoEscolhida.classList.remove('selecionado')
+    }
+
+    todos.classList.add("selecionado")
+
+    const opcaoEscolhidaUsuario = document.querySelector('.participantes .selecionado span').innerHTML
+}
+
+function escolherVisibilidade (opcaoVisibilidade) {
+    const opcaoEscolhida = document.querySelector('.visibilidade .selecionado')
+
+    if (opcaoEscolhida !== null){
+        opcaoEscolhida.classList.remove('selecionado')
+    }
+
+    opcaoVisibilidade.classList.add("selecionado")
+
+    textoInput()
+}
+
+function textoInput () {
+    const opcaoEscolhidaUsuario = document.querySelector('.participantes .selecionado span').innerHTML
+    const opcaoEscolhidaTexto = document.querySelector('.visibilidade .selecionado span').innerHTML
+
+    const textoInput = document.querySelector(".enviando")
+    textoInput.innerHTML = `Enviando mensagem para ${opcaoEscolhidaUsuario} (${opcaoEscolhidaTexto})`
 }
